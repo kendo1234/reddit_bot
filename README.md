9a12a9ebfed3756a4117946195d9fb18f831a83a
talbotk <kentalbot@rentalcars.com>
2018-04-05 08:57:55 +0100

    Notifier Bot finished and update README

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
+
+Bot:
+
 This bot currently pulls the top 25 comments from subreddit 'test' and for each instance of the word 'joke' responds
 with a joke using the Chuck Norris API.
-
 
 To Run:
 
@@ -13,3 +15,9 @@ client_id = ""
 client_secret = ""
 
 run from command line with - python r_bot.py
+
+Notifier:
+
+Uses file containing usernames to send a private message to a reddit user
+
+example useage: python notifier.py usernames.txt "Test comment" "Test comment body"
--- notifier.py
+++ notifier.py
@@ -1,19 +1,22 @@
 import praw
 import config
 import sys
+import time
 
+#Login function
 def authenticate():
-    #Log in using reddit credentials
+    # Log in using reddit credentials
     print("Logging in...")
     reddit = praw.Reddit(username=config.username,
-                    password=config.password,
-                    client_id=config.client_id,
-                    client_secret=config.client_secret,
-                    user_agent="reddit_notifier_bot by Ken /u/kendomustdie")
+                         password=config.password,
+                         client_id=config.client_id,
+                         client_secret=config.client_secret,
+                         user_agent="reddit_notifier_bot by Ken /u/kendomustdie")
     print("Authenticated as {}".format(reddit.user.me()))
 
     return reddit
 
+#Look for file usernames.txt
 def get_usernames(filename):
     try:
         with open(filename, "r") as f:
@@ -21,20 +24,34 @@ def get_usernames(filename):
             usernames = usernames.split("\n")
             usernames = filter(None, usernames)
     except IOError:
-        print("Error: File " + filename + " was not found int he current directory")
+        print("Error: File " + filename + " was not found in the current directory")
         quit()
 
-        return usernames
+    return usernames
 
-if not len(sys.argv) == 4:
-    print("usage: notifier_bot.py file \"subject\" \"body\"")
+#Send reddit PM based on arguments passed in at runtime, catch the exception if user not found
+def send_message(r, username, subject, body):
+    try:
+        r.redditor(username).message(subject, body)
+    except praw.exceptions.APIException as e:
+        if "USER_DOESNT_EXIST" in e.args[0]:
+            print("redditor " + username + " not found, did not send a message.")
+            return
 
-    filename = sys.argv[1]
-    subject = sys.argv[2]
-    body = sys.argv[3]
+    print("Sent message to " + username + "!")
+
+#Arguments to pass in when running
+if len(sys.argv) != 4:
+    print("usage: notifier.py file \"subject\" \"body\"")
+
+filename = sys.argv[1]
+subject = sys.argv[2]
+body = sys.argv[3]
 
 reddit = authenticate()
 usernames = get_usernames(filename)
-print(usernames)
 
-
+for username in usernames:
+    send_message(reddit, username, subject, body)
+    #Sleep after each send to avoid spamming
+    time.sleep(5)
